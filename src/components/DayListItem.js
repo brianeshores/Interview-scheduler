7ad581dd7ec8ec5ff06e70b8e5a8e13e8221ad44
Brian Murray <brianmurraysound@gmail.com>
2019-11-12 11:53:26 -0500

    Daylist component added and corresponding stories

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,6 +3,16 @@ import "components/DayListItem.scss";
 
 const classNames = require('classnames');
 
+const formatSpots = (spots) => {
+  if (spots > 1) {
+    return `${spots} spots remaining`;
+  } else if (spots === 1) {
+    return "1 spot remaining"
+  } else {
+    return "no spots remaining";
+  }
+}
+
 export default function DayListItem(props) {
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected":props.selected,
@@ -11,7 +21,7 @@ export default function DayListItem(props) {
   return (
     <li onClick={()=>props.setDay(props.name)} className={dayClass}>
       <h2 className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} days remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
