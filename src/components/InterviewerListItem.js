b95af9bd8187435d552f44e16dbff05f2fc3cad2
Brian Murray <brianmurraysound@gmail.com>
2019-11-13 15:40:55 -0500

    interviewer list item handling click events and changing styling

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,22 @@
 import React from "react";
-import "components/InterviewerListItem.scss"
+import "components/InterviewerListItem.scss";
+
+const classNames = require('classnames');
 
 export default function InterviewerListItem(props) {
+  
+  const interviewersClass = classNames("interviewers__item", {
+    "interviewers__item--selected":props.selected
+  });
+  
   return (
-    <li className="interviewers__item">
-      <img
-        className="interviewers__item-image"
-        src={props.avatar}
-        alt="Sylvia Palmer"
-      />
-      {props.name}
-    </li>  
-  )
+  <li className={interviewersClass} onClick={props.setInterviewer}>
+    <img
+      className="interviewers__item-image"
+      src={props.avatar}
+      alt={props.name}
+    />
+    {props.selected && props.name}
+  </li>
+  );
 }
