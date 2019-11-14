fb182c5e271ffb3368349889e4cfc359be4f5eda
Brian Murray <brianmurraysound@gmail.com>
2019-11-14 12:34:40 -0500

    Appointment component updated to conditionally render Empty or Show components

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -6,10 +6,21 @@ import Button from "components/Button";
 export default function Form(props) {
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
   const [name, setName] = useState(props.name || "");
+
+  const reset = () => {
+    setInterviewer(null);
+    setName("");
+  }
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  }
+  
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             onChange={(event) => setName(event.target.value)}
             className="appointment__create-input text--semi-bold"
@@ -25,7 +36,7 @@ export default function Form(props) {
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
           <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
