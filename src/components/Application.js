a3cef35a97480f0f11eb1d8aa032a00ff9114cd8
Brian Murray <brianmurraysound@gmail.com>
2019-12-02 17:52:30 -0500

    useApplicationData hook added to handle api data requests

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,58 +4,17 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment";
 import { getAppointmentsForDay, getInterview, getInterviewersByDay } from "helpers/selectors.js";
+import useApplicationData from "hooks/useApplicationsData.js"
 
 
 export default function Application(props) {
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
-  
+  const {
+    state,
+    setDay,
+    bookInterview,
+    deleteInterview
+  } = useApplicationData();
 
-   useEffect(() => {
-    Promise.all([Promise.resolve(axios.get("/api/days")),
-                 Promise.resolve(axios.get("/api/appointments")),
-                 Promise.resolve(axios.get("/api/interviewers"))])
-           .then((all) => {
-             setState(prev => ({day: "Monday", days: all[0].data, appointments: all[1].data, interviewers: all[2].data}))
-           })
-    }, []);
-
-  const setDay = day => {
-    setState({ ...state, day });
-  }
-
-
-  function bookInterview(id, interview) {
-    return axios.put("http://localhost:8001/api/appointments/"+id, 
-    {interview}).then( data => {
-      setState((prev) => {
-        return (
-          {...prev,
-            appointments:{...prev.appointments, [id]: {
-          ...prev.appointments[id], interview:interview
-          }}}
-        )
-      })
-    })
-  }
-
-  function deleteInterview(id, interview) {
-    return axios.delete("http://localhost:8001/api/appointments/"+id).then( data => {
-      setState((prev) => {
-        return (
-          {...prev,
-            appointments:{...prev.appointments, [id]: {
-              ...prev.appointments[id], interview:null
-          }}}
-        )
-      })
-    })
-  }
-  console.log("day spots", state.days[0])
   const appointmentArray = getAppointmentsForDay(state, state.day);
   const interviewersArray = getInterviewersByDay(state, state.day);
 
---
+++ src/hooks/useApplicationsData.js
@@ -0,0 +1,108 @@
+import React, { useReducer, useEffect } from "react";
+import axios from "axios"
+import {getAppointmentsForDay, } from "helpers/selectors"
+import reducer from "../reducer/application"
+
+let daysURL = "/api/days"
+let appointmentsURL = "/api/appointments"
+let interviewersURL = "/api/interviewers"
+
+
+export default function useApplicationData() {
+
+  const initialState = {
+    day:"Monday",
+    days: [],
+    appointments: {},
+    interviewers: {}
+  }
+
+  const SET_DAY = "SET_DAY";
+  const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
+  const SET_INTERVIEW = "SET_INTERVIEW";
+  const [state, dispatcher] = useReducer(reducer, initialState)
+
+  function bookInterview(id, interview) {
+    return axios.put("http://localhost:8001/api/appointments/"+id, {interview}).then( data => {
+      const days = state.days.map(day => {
+        day.appointments.includes(id) 
+        if(day.appointments.includes(id)){
+          return {
+            ...day,
+            spots: day.spots -1
+          }
+        } else {
+          return day
+        }
+      })
+        dispatcher({
+          type: SET_INTERVIEW,
+          value: {
+            appointments: {
+              ...state.appointments,
+              [id]: {
+                ...state.appointments[id], 
+                interview:interview
+              }
+            },
+            days
+          }
+        }
+      )
+    })
+  }
+
+function deleteInterview(id){
+  return axios.delete("http://localhost:8001/api/appointments/"+id).then( data => {
+    const days = state.days.map(day => {
+      day.appointments.includes(id) 
+      if(day.appointments.includes(id)){
+        return {
+          ...day,
+          spots: day.spots +1
+        }
+      } else {
+        return day
+      }
+    }) 
+  
+  dispatcher({
+      type: SET_INTERVIEW,
+      value: {
+        appointments: {
+          ...state.appointments,
+          [id]: {
+            ...state.appointments[id], interview:null
+          }
+        },
+        days
+      }
+    }
+  )
+})
+}
+
+function setDay(string) {
+dispatcher({type:SET_DAY, value:{...state, day:string}})
+}
+
+
+  useEffect(() => {
+    const promise1 = axios.get(daysURL);
+    const promise2 = axios.get(appointmentsURL);
+    const promise3 = axios.get(interviewersURL);
+  
+    Promise.all([promise1, promise2, promise3])
+      .then((all) => {
+        dispatcher({type: SET_APPLICATION_DATA, value: { days: all[0].data, appointments: all[1].data, interviewers: all[2].data }});
+    });
+  },[])
+
+  return {
+ state,
+ dispatcher,
+ bookInterview,
+ deleteInterview,
+ setDay,
+  }
+}
